7e60a6f45a456bb68fe27f6d5ed5e43574e56ca2
pablopascu99 <59370966+pablopascu99@users.noreply.github.com>
2020-02-24 13:10:05 +0100

    Add files via upload

---
+++ Cita.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Cita {
+    private LocalDateTime horaApertura;
+    private LocalDateTime horaCierre;
+    private Medico medicos;//asociacion
+    private Especialista especialistas;//asociacion
+    //private ArrayList<Paciente> pacientes;
+}
---
+++ Paciente.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class Paciente {
+	private int numTarjetaSanitaria;
+	private LocalDate fechaNacimiento;
+	private String DNI;
+}
